6f8e08e4a9b45b6cf948b697f15422994ba24d16
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2025-11-21 11:16:53 +0530

    modified nginx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:6000',
-        changeOrigin: true
-      }
-    }
+    host: true, // Allows LAN access when running `npm run dev`
+    port: 5173, // You can change this if needed
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
