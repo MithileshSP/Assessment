816b31657107f8577271c2c09515af00e4188e10
MithileshSP <mithileshsrinivasan2007@gmail.com>
2025-12-26 15:44:30 +0530

    push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/fullstack/'
-=======
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:6000',
-        changeOrigin: true
-      }
-    }
-  }
->>>>>>> 4abcbbc4511c75d6507e5492d9d4cf32b4a54f3f
 })
